ad567c85f874f9f256cca3c77a13be1d275a6192
Nikhil Sharma <NikhilSharma@example.com>
2024-03-03 16:30:46 +0530

    commit message

---
+++ email-service/email-service/src/main/java/com/nikhilsharma/emailservice/kafka/OrderConsumer.java
@@ -0,0 +1,23 @@
+package com.nikhilsharma.emailservice.kafka;
+
+import com.nikhilsharma.basedomains.dto.OrderEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderConsumer.class);
+
+    @KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
+    public void consume(OrderEvent event){
+        LOGGER.info(String.format("Order event recieved in email service => %s", event.toString()));
+
+        //save the order event into the database
+
+
+    }
+
+}
